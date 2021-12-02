912fe4327405dcfa29e7b29279a74871f236019c
Кузьмин Виктор <viksimurg@gmail.com>
2021-12-02 14:13:03 +0300

    init + fab

---
+++ app/src/main/java/ru/viksimurg/viknote/MainActivity.kt
@@ -0,0 +1,79 @@
+package ru.viksimurg.viknote
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import ru.viksimurg.viknote.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        setFabButtons()
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    fun setFabButtons(){
+        binding.fabMenu.shrink()
+        binding.fabMenu.setOnClickListener { view ->
+            expandOrCollapseFAB()
+        }
+
+    }
+
+    fun expandOrCollapseFAB(){
+        if(binding.fabMenu.isExtended){
+            binding.fabMenu.shrink()
+            binding.fabNote.hide()
+            binding.fabFolder.hide()
+            binding.fabLabelNote.visibility = View.GONE
+            binding.fabLabelFolder.visibility = View.GONE
+        }else{
+            binding.fabMenu.extend()
+            binding.fabNote.show()
+            binding.fabFolder.show()
+            binding.fabLabelNote.visibility = View.VISIBLE
+            binding.fabLabelFolder.visibility = View.VISIBLE
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
